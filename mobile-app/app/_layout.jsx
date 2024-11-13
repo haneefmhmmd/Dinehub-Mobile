55bc21832d547535147fd9c20fdecc99a0388e82
haneefMuhammad <haneefmhmmd@gmail.com>
2024-11-13 11:39:37 -0500

    fixed page not loading issue

--- mobile-app/app/_layout.jsx
+++ mobile-app/app/_layout.jsx
@@ -3,22 +3,30 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useCallback } from "react";
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, error] = useFonts({
     DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
     DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
 
+  if (error) {
+    console.error("Error loading fonts", error);
+  }
+
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
+      console.log("Font loaded!");
       await SplashScreen.hideAsync();
     }
   }, [fontsLoaded]);
 
-  if (!fontsLoaded) return null;
+  if (!fontsLoaded) {
+    console.log("Font Not loaded!");
+    return null;
+  }
 
   return <Stack onLayout={onLayoutRootView} />;
 };
